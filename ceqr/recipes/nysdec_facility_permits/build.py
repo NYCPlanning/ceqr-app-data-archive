c8a5b459938b3c43a8138b3223e2220f76497dd7
Baoling Zhou <baolingz@nyu.edu>
2019-10-25 16:16:02 +0000

    improve geocoding for dec permits

--- ceqr/recipes/nysdec_facility_permits/build.py
+++ ceqr/recipes/nysdec_facility_permits/build.py
@@ -11,34 +11,47 @@ import numpy as np
 import os
 
 def geocode(inputs):
-    hnum = inputs.pop('hnum')
-    sname = inputs.pop('sname')
-    zip_code = inputs.pop('zip_code')
+    hnum = inputs.get('housenum', '')
+    sname = inputs.get('streetname', '')
+    zip_code = inputs.get('zipcode', '')
 
     hnum = str('' if hnum is None else hnum)
     sname = str('' if sname is None else sname)
     zip_code = str('' if zip_code is None else zip_code)
+
     try:
         geo = g['1B'](street_name=sname, house_number=hnum, zip_code=zip_code)
-    except GeosupportError as e:
-        geo = e.result
-
-    geo = parser(geo)
-    geo.update(inputs)
-    return geo
+        geo = parser(geo)
+        geo.update(inputs)
+        return geo
+    except GeosupportError:
+        try:
+            geo = g['AP'](street_name=sname, house_number=hnum, zip_code=zip_code)
+            geo = parser(geo)
+            geo.update(inputs)
+            return geo
+        except GeosupportError as e:
+            geo = parser(e.result)
+            geo.update(inputs)
+            return geo
 
 def parser(geo):
     return dict(
-        house_number = geo.get('House Number - Display Format', ''),
-        street_name = geo.get('First Street Name Normalized', ''),
-        bbl = geo.get('BOROUGH BLOCK LOT (BBL)', {}).get('BOROUGH BLOCK LOT (BBL)', '',),
-        bin = geo.get('Building Identification Number (BIN) of Input Address or NAP', ''),
-        latitude = geo.get('Latitude', ''),
-        longitude = geo.get('Longitude', ''),
-        grc = geo.get('Geosupport Return Code (GRC)', ''),
+        geo_housenum = geo.get('House Number - Display Format', ''),
+        geo_streetname = geo.get('First Street Name Normalized', ''),
+        geo_bbl = geo.get('BOROUGH BLOCK LOT (BBL)', {}).get('BOROUGH BLOCK LOT (BBL)', '',),
+        geo_bin = geo.get('Building Identification Number (BIN) of Input Address or NAP', ''),
+        geo_latitude = geo.get('Latitude', ''),
+        geo_longitude = geo.get('Longitude', ''),
+        geo_grc = geo.get('Geosupport Return Code (GRC)', ''),
     )
-def test(a):
-    return {'a':'1', 'b':'2'}
+
+def clean_address(x):
+    if x != None:
+        sep = ['|', '&', '@', ' AND ']
+        for i in sep:
+            x = x.split(i, maxsplit=1)[0]
+    return x
 
 if __name__ == "__main__":
     # Load configuration
@@ -57,9 +70,11 @@ if __name__ == "__main__":
     df = dec_state_facility_permits.append(dec_title_v_facility_permits)
 
     # geocoding ... with 1E
-    df['hnum'] = df['facility_location'].apply(get_hnum)
-    df['sname'] = df['facility_location'].apply(get_sname)
-    df['zip_code'] = df['facility_zip']
+    df.rename(columns={"facility_zip": "zipcode", "expire_date": "expiration_date"}, inplace=True)
+    df['address'] = df['facility_location'].apply(lambda x: clean_address(x))
+    df['housenum'] = df['address'].apply(get_hnum)\
+                                  .apply(lambda x: x.split('/',maxsplit=1)[0] if x != None else x)
+    df['streetname'] = df['address'].apply(get_sname)
     records = df.to_dict('records')
 
     # Multiprocess
@@ -67,31 +82,34 @@ if __name__ == "__main__":
         it = pool.map(geocode, records, 10000)
 
     df = pd.DataFrame(it)
-    df = df[df['grc'] != '71']
-
-    df['longitude'] = pd.to_numeric(df['longitude'],errors='coerce')
-    df['latitude'] = pd.to_numeric(df['latitude'],errors='coerce')
-    df = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude))
-    df['geometry'] = df['geometry'].apply(lambda x: None if np.isnan(x.xy[0]) else str(x))
+    df = df[df['geo_grc'] != '71']
+    df['geo_address'] = None
+    df['geo_longitude'] = pd.to_numeric(df['geo_longitude'],errors='coerce')
+    df['geo_latitude'] = pd.to_numeric(df['geo_latitude'],errors='coerce')
+    df = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.geo_longitude, df.geo_latitude))
+    df['geom'] = df['geometry'].apply(lambda x: None if np.isnan(x.xy[0]) else str(x))
 
     SQL = f'''
+        UPDATE {output_table} SET geo_address=geo_housenum||' '||geo_streetname;
+
         ALTER TABLE {output_table}
         ADD COLUMN id SERIAL PRIMARY KEY;
 
         DELETE FROM {output_table}
         WHERE id NOT IN(
             WITH date AS(
-                SELECT facility_name||facility_location AS facility, MAX(issue_date::date) as latest_issue_date
+                SELECT facility_name||address AS facility, MAX(issue_date::date) as latest_issue_date
                 FROM {output_table}
-                GROUP BY facility_name||facility_location
+                GROUP BY facility_name||address
             )
             SELECT min(id)
             FROM {output_table} p, date d
-            WHERE p.facility_name||facility_location = d.facility
+            WHERE p.facility_name||address = d.facility
             AND p.issue_date::date = d.latest_issue_date
             OR d.latest_issue_date IS NULL
-            GROUP BY p.facility_name||facility_location
-        );
+            GROUP BY p.facility_name||address
+        )
+        OR geom IS NULL;
 
         ALTER TABLE {output_table} DROP COLUMN id;
         '''
@@ -103,4 +121,4 @@ if __name__ == "__main__":
              DDL=DDL,
              sql=SQL,
              sep='~',
-             geo_column='geometry')
+             geo_column='geom')
