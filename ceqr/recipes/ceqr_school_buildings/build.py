c3bcb5e053a49e24f3f7868f6b49eda786f8a4d3
Baoling Zhou <baolingz@nyu.edu>
2020-01-07 22:11:10 +0000

    update ceqr_school_buildings 2019

--- ceqr/recipes/ceqr_school_buildings/build.py
+++ ceqr/recipes/ceqr_school_buildings/build.py
@@ -7,46 +7,51 @@ from pathlib import Path
 import numpy as np
 import os
 
-def get_boro(d):
-
-    BORO = {
-        1: list(range(1,7)),
-        2: list(range(7,13)),
-        3: list(range(13,24))+[32],
-        4: list(range(24,31)),
-        5: [31]
-    }
-    for boro, district in BORO.items():
-        if int(d) in district:
-            return boro
-
 if __name__ == "__main__":
     # Load configuration
     config = load_config(Path(__file__).parent/'config.json')
-    input_table_lcgms = config['inputs'][0]
-    input_table_bluebook = config['inputs'][1]
+    input_table_bluebook = config['inputs'][0]
+    input_table_lcgms = config['inputs'][1]
     input_table_subdistricts = config['inputs'][2]
+    input_table_boro = config['inputs'][3]
     output_table = config['outputs'][0]['output_table']
     DDL = config['outputs'][0]['DDL']
 
     # import data
-    bluebook = gpd.GeoDataFrame.from_postgis(f'''SELECT *, 'bluebook' AS source, \
-                                                ST_TRANSFORM(ST_SetSRID(ST_MakePoint(x::NUMERIC, y::NUMERIC),2263),4326) AS geom \
-                                                FROM {input_table_bluebook}''', con=recipe_engine, geom_col='geom')
-    lcgms = gpd.GeoDataFrame.from_postgis(f'''SELECT *, 'lcgms' AS source FROM {input_table_lcgms}''',
-                                                    con=recipe_engine, geom_col='geom')
-    doe_school_subdistrict = gpd.GeoDataFrame.from_postgis(f'SELECT * FROM {input_table_subdistricts}', 
+    bluebook = gpd.GeoDataFrame.from_postgis(f'''
+                                                SELECT a.*, b.borocode, 'bluebook' AS source,\
+                                                ST_TRANSFORM(ST_SetSRID(ST_MakePoint(a.x::NUMERIC, a.y::NUMERIC),2263),4326) AS geom\
+                                                FROM {input_table_bluebook} a, {input_table_boro} b\
+                                                WHERE ST_Within(ST_TRANSFORM(ST_SetSRID(ST_MakePoint(a.x::NUMERIC, a.y::NUMERIC),2263),4326),\
+                                                    b.wkb_geometry)\
+                                                AND a.org_id IS NOT NULL\
+                                                ''', con=recipe_engine, geom_col='geom')
+
+    #load lcgms records, excluding outsideNYC and ones existing in bluebook already
+    lcgms = gpd.GeoDataFrame.from_postgis(f'''SELECT a.*, b.borocode, 'lcgms' AS source,\
+                                                ST_SetSRID(ST_MakePoint(a.longitude::NUMERIC, a.latitude::NUMERIC),4326) AS geom \
+                                                FROM {input_table_lcgms} a, {input_table_boro} b\
+                                                WHERE ST_Within(ST_SetSRID(ST_MakePoint(a.longitude::NUMERIC, a.latitude::NUMERIC),4326),\
+                                                    b.wkb_geometry)\
+                                                AND latitude !~* '^0'\
+                                                AND building_code||location_code NOT IN (\
+                                                    SELECT DISTINCT bldg_id||org_id\
+                                                    FROM {input_table_bluebook}\
+                                                    WHERE bldg_id||org_id IS NOT NULL)\
+                                                ''', con=recipe_engine, geom_col='geom')
+    subdistrict = gpd.GeoDataFrame.from_postgis(f'SELECT * FROM {input_table_subdistricts}',
                                                     con=ceqr_engine, geom_col='geom')
     
+    ## apply filter
+    # bluebook = bluebook[bluebook.org_level.isin(['PS','IS','HS','PSIS','ISHS'])]
+
     # perform column transformation for bluebook
-    bluebook = bluebook[bluebook.org_level.isin(['PS','IS','HS','PSIS','ISHS'])]
     bluebook.rename(columns={'bldg_excl.': 'excluded', 'organization_name':'name'}, inplace = True)
-    bluebook['borocode'] = bluebook.district.apply(lambda x: get_boro(x))
     bluebook['excluded'] = bluebook.excluded.apply(lambda x: True if x == 'Y' else False)
     bluebook['org_e'] = bluebook['org_e'].astype(float)
-    bluebook['ps_%'] = bluebook['ps_%'].apply(lambda x: float(x[:-1])/100).astype(float)
-    bluebook['ms_%'] = bluebook['ms_%'].apply(lambda x: float(x[:-1])/100).astype(float)
-    bluebook['hs_%'] = bluebook['hs_%'].apply(lambda x: float(x[:-1])/100).astype(float)
+    bluebook['ps_%'] = bluebook['ps_%'].apply(lambda x: float(x[:-1])/100 if x != None else None).astype(float)
+    bluebook['ms_%'] = bluebook['ms_%'].apply(lambda x: float(x[:-1])/100 if x != None else None).astype(float)
+    bluebook['hs_%'] = bluebook['hs_%'].apply(lambda x: float(x[:-1])/100 if x != None else None).astype(float)
     bluebook['pc'] = bluebook['pc'].fillna(0).astype(float).astype(int)
     bluebook['ic'] = bluebook['ic'].fillna(0).astype(float).astype(int)
     bluebook['hc'] = bluebook['hc'].fillna(0).astype(float).astype(int)
@@ -57,23 +62,28 @@ if __name__ == "__main__":
     bluebook['he'] = bluebook.org_e * bluebook['hs_%']
     bluebook['he'] = bluebook['he'].fillna(0).astype(int)
 
-    # # only keep the records from lcgms not existing in bluebook
-    # lcgms = lcgms[~(lcgms.org_id+lcgms.bldg_id).isin(bluebook.org_id+bluebook.bldg_id)]
+    # rename the column names
+    lcgms.rename(columns={'geographical_district_code': 'district',
+                          'building_name':'bldg_name',
+                          'building_id_number_(bin)':'bldg_id',
+                          'location_code':'org_id',
+                          'location_name':'name'}, inplace = True)
 
-    # # perform spatial join between lcgms and doe_school_subdistrict shapefile
-    # lcgms = gpd.sjoin(lcgms, doe_school_subdistrict[['district','subdistrict', 'geom']], op='within')
-    # lcgms = pd.DataFrame(lcgms)
+    # perform spatial join between lcgms and doe_school_subdistrict shapefile
+    lcgms = gpd.sjoin(lcgms, subdistrict[['subdistrict', 'geom']], op='within')
 
-    # # perform column transformation for lcgms
-    # lcgms['borocode'] = lcgms.bbl.apply(lambda x: str(x)[0]).astype(int)
-    # lcgms['bldg_name'] = lcgms.name
-    # lcgms['excluded'] = False
-    # lcgms['pc'] = 0
-    # lcgms['ic'] = 0
-    # lcgms['hc'] = 0
+    # perform column transformation for lcgms
+    lcgms['excluded'] = False
+    lcgms['pc'] = 0
+    lcgms['ic'] = 0
+    lcgms['hc'] = 0
+    lcgms['pe'] = 0
+    lcgms['ie'] = 0
+    lcgms['he'] = 0
+    lcgms['org_level'] = None
 
-    # # merge lcgms and bluebook
-    df = bluebook.iloc[:,:]
+    # merge lcgms and bluebook
+    df = bluebook[DDL.keys()].append(lcgms[DDL.keys()])
     
     os.system('echo "exporting table ..."')
     # export table to EDM_DATA
